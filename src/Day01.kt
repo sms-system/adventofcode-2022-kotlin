f098e7c7edfb3385659078da06b004b45f19768d
Serge Smirnov <sms-system@mail.ru>
2022-12-01 18:08:36 +0600

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,32 @@
 fun main() {
+    fun getListOfCalories(input: List<String>): List<Int> {
+        var acc: Int = 0
+        val data = input.fold(listOf<Int>()) { a, b ->
+            if (b == "") {
+                val ret = a + acc
+                acc = 0
+                ret
+            } else {
+                acc += b.toInt()
+                a
+            }
+        }
+        return data + acc
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val listOfCalories = getListOfCalories(input)
+        return listOfCalories.maxOrNull()?:0
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val listOfCalories = getListOfCalories(input).sortedDescending()
+        return listOfCalories[0] + listOfCalories[1] + listOfCalories[2]
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
